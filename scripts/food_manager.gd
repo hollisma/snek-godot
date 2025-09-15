19702aa482362da76a2ee3c45a896279d0a01930
Hollis Ma <hollisleroyma@gmail.com>
2025-09-15 14:25:37 -0400

    fade golden appl then spawn new appl

--- scripts/food_manager.gd
+++ scripts/food_manager.gd
@@ -4,8 +4,12 @@ var appl_scene: PackedScene = preload("res://scenes/appl.tscn")
 var appl_green_scene: PackedScene = preload("res://scenes/appl_green.tscn")
 var appl_golden_scene: PackedScene = preload("res://scenes/appl_golden.tscn")
 
+const GOLDEN_APPL_TIME = 5.0 # 5.0 secs
+
 var snek_head: Node
+var golden_appl: Node
 @onready var screen_size = get_viewport().size
+@onready var golden_appl_timer = $GoldenApplTimer
 
 func spawn_appl(): 
 	var margin = 16
@@ -25,9 +29,24 @@ func spawn_appl():
 
 func _get_appl_instance() -> Node:
 	var rand = randi() % 100
-	var scene = appl_scene
+	var appl: Node
 	if rand < 15: # 15% chance to be green
-		scene = appl_green_scene
+		appl = appl_green_scene.instantiate()
 	elif rand < 20: # 5% chance to be golden
-		scene = appl_golden_scene
-	return scene.instantiate()
+		appl = appl_golden_scene.instantiate()
+		start_golden_appl(appl)
+	else: 
+		appl = appl_scene.instantiate()
+	return appl
+
+func start_golden_appl(appl): 
+	golden_appl = appl
+	var tween = create_tween()
+	tween.tween_property(appl, "modulate:a", 0.0, GOLDEN_APPL_TIME)
+	golden_appl_timer.start(GOLDEN_APPL_TIME)
+
+func _on_GoldenApplTimer_timeout():
+	if golden_appl and is_instance_valid(golden_appl): 
+		golden_appl.queue_free()
+		golden_appl = null
+		spawn_appl()
